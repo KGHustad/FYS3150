b4875ff1e33d6fe966ff13e037b8d38fc2cb3aaf
Jonas Lunde <jonas.s.lunde@outlook.com>
2016-09-02 16:04:33 +0200

    task c completed

---
+++ Project1/task_c.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def u_func(x):
+	return 1-(1-np.exp(-10))*x-np.exp(-10*x)
+
+def f_func(x):
+	return 100*np.exp(-10*x)
+
+def solve_specific(f_func, n):
+	x = np.linspace(0,1,n+2)
+	h = x[1] - x[0]
+	f = f_func(x)*h**2
+	u = np.zeros(n+2)
+	a_func = lambda i: (i+1)/i
+	a = a_func(np.arange(0,n+1, dtype=np.float64))
+	a[0] = 0 #fysiker-løsning
+	for i in range(2,n+1):
+		f[i] = f[i] + f[i-1]/a[i-1]	# 2 FLOPS
+	
+	u[n] = f[n]/float(a[n])
+
+	for i in range(n-1,0,-1):
+		u[i] = (f[i]+u[i+1])/a[i]	# 2 FLOPS
+
+	return u, x
+
+def plot_func(f_func, n):
+	u, x = solve_specific(f_func, n)
+	plt.plot(x, u, x, u_func(x))
+
+for n in ([10,100,1000]):
+	a = np.full(n+2, 2, dtype=np.float64)
+	b = np.full(n+2, -1, dtype=np.float64)
+	c = np.full(n+2, -1, dtype=np.float64)
+	plot_func(f_func, n)
+plt.show()
