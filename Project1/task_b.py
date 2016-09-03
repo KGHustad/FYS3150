ba1114eabbc91ee6cb874c45e8382bbe958cd26a
Jonas Lunde <jonas.s.lunde@outlook.com>
2016-09-03 16:51:03 +0200

    Added exercise d to the task_d file and swtiched numerical u to v

--- Project1/task_b.py
+++ Project1/task_b.py
@@ -11,23 +11,23 @@ def solve_general(f_func, n, a, b, c):
 	x = np.linspace(0, 1, n+2)
 	h = x[1] - x[0]
 	f = f_func(x)*h**2
-	u = np.zeros(n+2)
+	v = np.zeros(n+2)
 
 	for i in range(2,n+1): #Forward Substitution
-		a[i] = a[i] - (b[i-1]*c[i])/a[i-1]	# 3 FLOPS
-		f[i] = f[i] - f[i-1]*c[i]/a[i-1]	# 3 FLOPS
+		a[i] = a[i] - (b[i-1]*c[i])/a[i-1]	# 3 FLOP
+		f[i] = f[i] - f[i-1]*c[i]/a[i-1]	# 3 FLOP
 
 	#Backward Substitution
-	u[n] = f[n]/float(a[n])
+	v[n] = f[n]/float(a[n])
 
 	for i in range(n-1,0,-1):
-		u[i] = (f[i] - b[i]*u[i+1]) / a[i]	# 3 FLOPS
+		v[i] = (f[i] - b[i]*v[i+1]) / a[i]	# 3 FLOP
 
-	return u, x
+	return v, x
 
 def plot_func(f_func, n, a, b, c):
-	u, x = solve_general(f_func, n, a, b, c)
-	plt.plot(x, u, x, u_func(x))
+	v, x = solve_general(f_func, n, a, b, c)
+	plt.plot(x, v, x, u_func(x))
 
 for n in ([10,100,1000]):
 	a = np.full(n+2, 2, dtype=np.float64)
--- Project1/task_c.py
+++ Project1/task_c.py
@@ -11,30 +11,33 @@ def solve_specific(f_func, n):
 	x = np.linspace(0, 1, n+2)
 	h = x[1] - x[0]
 	f = f_func(x)*h**2
-	u = np.zeros(n+2)
+	v = np.zeros(n+2)
 	a_func = lambda i: (i+1)/i
 	a = a_func(np.arange(0,n+1, dtype=np.float64))
 	a[0] = 0 #fysiker loesning
 	for i in range(2,n+1):
-		f[i] = f[i] + f[i-1]/a[i-1]	# 2 FLOPS
+		f[i] = f[i] + f[i-1]/a[i-1]	# 2 FLOP
 	
-	u[n] = f[n]/float(a[n])
+	v[n] = f[n]/float(a[n])
 
 	for i in range(n-1,0,-1):
-		u[i] = (f[i]+u[i+1])/a[i]	# 2 FLOPS
+		v[i] = (f[i]+v[i+1])/a[i]	# 2 FLOP
 
-	return u, x
+	return v, x
 
 def plot_func(f_func, n):
-	u, x = solve_specific(f_func, n)
-	plt.plot(x, u, x, u_func(x))
+	v, x = solve_specific(f_func, n)
+	plt.plot(x, v, x, u_func(x))
 
+"""
 for n in ([10,100,1000]):
 	a = np.full(n+2, 2, dtype=np.float64)
 	b = np.full(n+2, -1, dtype=np.float64)
 	c = np.full(n+2, -1, dtype=np.float64)
 	plot_func(f_func, n)
 plt.show()
+"""
+
 
 def CPU_time(n):
 	import time
@@ -45,11 +48,23 @@ def CPU_time(n):
 	c = np.full(n+2, -1, dtype=np.float64)
 
 	start_time = time.time()
-	u,x = solve_general(f_func, n, a, b, c)
+	v,x = solve_general(f_func, n, a, b, c)
 	print "General function run-time: %f seconds" % (time.time() - start_time)
 	
 	start_time = time.time()
-	u,x = solve_specific(f_func, n)
+	v,x = solve_specific(f_func, n)
 	print "Specific function run-time: %f seconds" % (time.time() - start_time)
 
-CPU_time(10**6)
+#CPU_time(10**6)
+
+
+def error_solver(i):
+	for n in (i):
+		v, x = solve_specific(f_func, n)
+		u = u_func(x) 
+		eps_i = np.log10(abs(np.divide((v[1:-1]-u[1:-1]),u[1:-1]))) #The first and last arguments are the boundery conditions in 0 and n+1, which are outside the matrix, and we don't want them.
+		eps = np.max(eps_i)
+		print "Logaritmic relative error for n = %e: %f" % (n, eps)
+	
+
+error_solver([10,10**2,10**3,10**4,10**5,10**6])
