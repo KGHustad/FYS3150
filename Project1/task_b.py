2b135444f3b87d075d313118373cd0020d1ccf81
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-09-14 17:53:35 +0200

    Enforced variable consistency and finished the derivation of the specialized algorithm in the report

--- Project1/task_b.py
+++ Project1/task_b.py
@@ -13,30 +13,28 @@ def solve_general(f_func, n, a, b, c):
 
     # Forward Substitution
     for i in range(2, n+1):
-        row_factor = c[i]/a[i-1]      # 1 FLOP
-        a[i] -= b[i-1]*row_factor     # 2 FLOPS
+        row_factor = a[i]/b[i-1]      # 1 FLOP
+        b[i] -= c[i-1]*row_factor     # 2 FLOPS
         s[i] -= s[i-1]*row_factor     # 2 FLOPS
 
     #Backward Substitution
-    v[n] = s[n]/a[n]
+    v[n] = s[n]/b[n]
 
     for i in range(n-1,0,-1):
-        v[i] = (s[i] - b[i]*v[i+1]) / a[i]  # 3 FLOPS
+        v[i] = (s[i] - c[i]*v[i+1]) / b[i]  # 3 FLOPS
 
     return x, v
 
-def plot_func(f_func, n, a, b, c, legend=None):
-    x, v = solve_general(f_func, n, a, b, c)
-    plt.plot(x, v)
 
 if __name__ == '__main__':
     # find and plot numerical approximations
     n_values = [10, 100, 1000]
     for n in n_values:
-        a = np.full(n+2, 2, dtype=np.float64)
-        b = np.full(n+2, -1, dtype=np.float64)
+        a = np.full(n+2, -1, dtype=np.float64)
+        b = np.full(n+2,  2, dtype=np.float64)
         c = np.full(n+2, -1, dtype=np.float64)
-        plot_func(f_func, n, a, b, c)
+        x, v = solve_general(f_func, n, a, b, c)
+        plt.plot(x, v)
     legend = ["N = %d" % n for n in n_values]
 
     # plot analytical solution
@@ -49,7 +47,6 @@ if __name__ == '__main__':
     plt.xlabel('x')
     plt.ylabel('u')
 
-    #plt.savefig('fig/plot_b.png')
     plt.savefig('fig/plot_b.pdf')
     if not "--dont_show" in sys.argv:
         plt.show()
--- Project1/task_c.py
+++ Project1/task_c.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 from common import *
 from task_b import solve_general
@@ -10,37 +11,27 @@ def solve_specific(f_func, n):
     h = x[1] - x[0]
     s = f_func(x)*h**2
     v = np.zeros(n+2)
-    a_func = lambda i: (i+1)/i
-    a = a_func(np.arange(0,n+1, dtype=np.float64))
-    a[0] = 0 #fysiker loesning
+    b_func = lambda i: (i+1)/i
+    b = np.zeros(n+2, dtype=np.float64)
+    i = np.arange(0, n+2, dtype=np.float64)
+    b[1:-1] = b_func(i[1:-1])
     for i in range(2,n+1):
-        s[i] = s[i] + s[i-1]/a[i-1] # 2 FLOP
+        s[i] = s[i] + s[i-1]/b[i-1] # 2 FLOPS
 
-    v[n] = s[n]/float(a[n])
+    v[n] = s[n]/b[n]
 
     for i in range(n-1,0,-1):
-        v[i] = (s[i]+v[i+1])/a[i]   # 2 FLOP
+        v[i] = (s[i]+v[i+1])/b[i]   # 2 FLOPS
 
     return x, v
-
-def plot_func(f_func, n):
-    x, v = solve_specific(f_func, n)
-    plt.plot(x, v, x, u_func(x))
-
-for n in ([10,100,1000]):
-    a = np.full(n+2, 2, dtype=np.float64)
-    b = np.full(n+2, -1, dtype=np.float64)
-    c = np.full(n+2, -1, dtype=np.float64)
-    plot_func(f_func, n)
-plt.show()
 
 
 def CPU_time(n):
     import time
     from task_b import solve_general
 
-    a = np.full(n+2, 2, dtype=np.float64)
-    b = np.full(n+2, -1, dtype=np.float64)
+    a = np.full(n+2, -1, dtype=np.float64)
+    b = np.full(n+2, 2, dtype=np.float64)
     c = np.full(n+2, -1, dtype=np.float64)
 
     start_time = time.time()
@@ -62,4 +53,29 @@ def error_solver(i):
         print "Logaritmic relative error for n = %e: %f" % (n, eps)
 
 
-error_solver([10,10**2,10**3,10**4,10**5,10**6])
+if __name__ == '__main__':
+    n_values = [10,100,1000]
+    for n in n_values:
+        a = np.full(n+2, 2, dtype=np.float64)
+        b = np.full(n+2, -1, dtype=np.float64)
+        c = np.full(n+2, -1, dtype=np.float64)
+        x, v = solve_specific(f_func, n)
+        plt.plot(x, v)
+    legend = ["N = %d" % n for n in n_values]
+
+    # plot analytical solution
+    x = np.linspace(0, 1, 1000)
+    plt.plot(x, u_func(x))
+    legend.append('Analytical sol.')
+
+    # make plot look great
+    plt.legend(legend)
+    plt.xlabel('x')
+    plt.ylabel('u')
+
+    plt.savefig('fig/plot_c.pdf')
+
+    if not '--dont_show' in sys.argv:
+        plt.show()
+
+    error_solver([10,10**2,10**3,10**4,10**5,10**6])
