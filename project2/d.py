fd9ec0b4ffbe08d2976b0b00f498e32b8e581e85
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-09-23 14:30:01 +0200

    b) and d) program done-ish

---
+++ project2/d.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sys
+
+from common import *
+
+if __name__ == '__main__':
+    n = 40
+    if len(sys.argv) > 1:
+        n = float(sys.argv[1])
+    omega_values = [0.01, 0.5, 1, 5]
+    for omega in omega_values:
+        A = make_matrix_interacting_case(n, omega)
+        R = np.eye(n)
+        solve(A, R)
+        eig_and_shit = sorted(A[range(n), range(n)])
+        print eig_and_shit#[4:40:8]
+    #print R
