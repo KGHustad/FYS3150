300c455e369535502492150645c10c88880142be
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-09-23 22:42:04 +0200

    Removed debug code

--- project2/e.py
+++ project2/e.py
@@ -5,9 +5,6 @@ from operator import itemgetter
 
 from common import *
 
-def sorted_matrix(B):
-    A = B.copy()
-    return A[A[0,:].argsort()]
 
 if __name__ == '__main__':
     n = 40
@@ -18,40 +15,13 @@ if __name__ == '__main__':
 
     solve(A, R)
 
+    # extract eigenvalues and eigenvectors
     eigvals = A[range(n), range(n)]
     eigvecs = [np.transpose(R)[i] for i in xrange(n)]
     eigs = [(eigval, eigvec, i) for i, (eigval, eigvec) in enumerate(zip(eigvals, eigvecs))]
     sorted_eigs = sorted(eigs, key=itemgetter(0))
 
-
-    #k= 4
-    #plt.plot(rho[1:], R[k][:])
-    #plt.plot(rho[1:], R[:][k])
-    #plt.plot(rho[1:], diff)
-    #plt.show()
-
-    """
-    A2, rho = make_matrix_noninteraction_case(n)
-    w, v = np.linalg.eig(A2)
-    np_eigvec = sorted_matrix(v)
-    our_eigvec = sorted_matrix(R)
-    diff_eigvec = our_eigvec - np_eigvec
-    max_diff_eigvec = abs(diff_eigvec).max(axis=(0,1))
-    if max_diff_eigvec > 1E-8:
-        #print "Numpy's eigvecs\n", v
-        #print "Our eigvecs\n", R
-        print "Max diff = %g" % max_diff_eigvec
-    else:
-        print "Numpy agrees with our eigenvectors (max diff = %g)" % max_diff_eigvec
-    """
-
-    #kk = 0
-    #plt.plot(rho[1:], v[kk][:])
-    #plt.plot(rho[1:], R[kk][:])
-    #plt.legend(['r','c'])
-    #plt.show()
-
-    #"""
+    # plot
     legend = []
     for i in xrange(3):
         plt.plot(rho[1:], sorted_eigs[i][1]**2)
@@ -60,10 +30,3 @@ if __name__ == '__main__':
     plt.xlabel('rho')
     plt.title('The three lowest energy levels.   n=%g' % n)
     plt.savefig('fig/plot_e.pdf')
-    #"""
-
-    #print eigvals
-    #print eigvecs
-
-    #eig_and_shit = sorted(A[range(n), range(n)])
-    #print eig_and_shit#[4:40:8]
