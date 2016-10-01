41c501022db1af065684398b0054bdd76664a047
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-10-02 00:47:55 +0200

    Forgot to add plotting program for b)

---
+++ project2/src/plot_b.py
@@ -0,0 +1,10 @@
+from common import *
+from main import *
+
+if __name__ == '__main__':
+    solver = 'c'
+    n = 200
+    rho_max = 5
+    for interaction in [False, True]:
+        plot_lowest_energy_levels(n, interaction, omega, rho_max, solver=solver,
+                                  plot=True, show=show, silent=True)
