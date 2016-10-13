4af23635a59b950203b92655d32573df15d2fbf6
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-10-13 09:15:48 +0200

    Locked aspect ratio so that a circular orbit appears circular on the plot

--- project3/solarsystem.py
+++ project3/solarsystem.py
@@ -5,10 +5,11 @@ import numpy as np
 MySolarSystem = SolarSystem()
 
 MySolarSystem.CreateCelestialObject(0, 0, 0, 0, 1, 1)
-MySolarSystem.CreateCelestialObject(1, 0, 0, 2*np.pi, 0.0000001, 1)
+MySolarSystem.CreateCelestialObject(1, 0, 0, 2.8*np.pi, 0.0000001, 1)
 MySolarSystem.CreateCelestialObject(0, 4, np.pi, 0, 0.0000001, 1)
-p, v = MySolarSystem.FillArray(100, 10)
+p, v = MySolarSystem.FillArray(20000, 200)
 
+plt.axes(aspect='equal')
 plt.plot(p[:,0,0], p[:,0,1], "yo")
 plt.plot(p[:,1,0], p[:,1,1], "r-")
 plt.plot(p[:,2,0], p[:,2,1], "b-")
