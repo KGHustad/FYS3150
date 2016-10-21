4cfd9b35c115e19a5ffe5ca2e528aa4c9df3c4d4
Jonas Gahr Sturtzel Lunde <jonas.s.lunde@outlook.com>
2016-10-21 14:06:37 +0200

    dunno

--- project3/EinsteinWasRightAllAlong.py
+++ project3/EinsteinWasRightAllAlong.py
@@ -12,31 +12,35 @@ MySolarSystem = SolarSystem()
 MySolarSystem.CreateCelestialObject(0, 0, 0, 0, 1)
 MySolarSystem.CreateCelestialObject(mercury_perihelion_distance, 0, 0, mercuryspeed, mercurymass)
 
-p_newton, v_newton = MySolarSystem.fill_array_c(100000, 100)
+p_newton, v_newton = MySolarSystem.fill_array_c(30000000, 100)
 
-p_einstein, v_einstein = MySolarSystem.fill_array_c(100000, 100, acc_method=MySolarSystem.AccRelativistic)
+p_einstein, v_einstein = MySolarSystem.fill_array_c(30000000, 100, acc_method=MySolarSystem.AccRelativistic)
 
 
 
-local_min_indexes = scipy.signal.argrelextrema(np.linalg.norm(p_newton[:,1],axis=1),np.less)[0]
+local_min_indexes = scipy.signal.argrelextrema( np.sqrt((p_newton[:,1,0] - p_newton[:,0,0])**2 + (p_newton[:,1,1] - p_newton[:,0,1])**2), np.less )[0]
+perihelion_points = np.zeros(shape=(len(local_min_indexes),2))
 perihelion_angle = np.zeros(len(local_min_indexes))
 for i in range(len(local_min_indexes)):
+    perihelion_points[i] = p_newton[local_min_indexes[i],1]
     perihelion_angle[i] = np.arctan( p_newton[local_min_indexes[i],1,1] / p_newton[local_min_indexes[i],1,0] )
-plt.plot(perihelion_angle)
+plt.plot(perihelion_angle, "r-")
 
 local_min_indexes = scipy.signal.argrelextrema(np.linalg.norm(p_newton[:,1],axis=1),np.less)[0]
-perihelion_angle = np.zeros(len(local_min_indexes))
+perihelion_angle = np.zeros(shape=(len(local_min_indexes),2))
 for i in range(len(local_min_indexes)):
+    perihelion_points[i] = p_einstein[local_min_indexes[i],1]
     perihelion_angle[i] = np.arctan( p_einstein[local_min_indexes[i],1,1] / p_einstein[local_min_indexes[i],1,0] )
-plt.plot(perihelion_angle)
+plt.plot(perihelion_angle, "b-")
 plt.show()
 
 
 plt.axes(aspect='equal')
-plt.plot(p_newton[:,0,0], p_newton[:,0,1], "yo")
-plt.plot(p_newton[99800:,1,0], p_newton[99800:,1,1], "r-")
-plt.plot(p_einstein[99800:,1,0], p_einstein[99800:,1,1], "b-")
+plt.plot(0,0, "yo")
+plt.plot(p_newton[::100,1,0], p_newton[::100,1,1], "r-")
+plt.plot(p_einstein[::100,1,0], p_einstein[::100,1,1], "b-")
 plt.plot(p_newton[-1,1,0], p_newton[-1,1,1], "ro")
 plt.plot(p_einstein[-1,1,0], p_einstein[-1,1,1], "bo")
-plt.plot()
+plt.plot(p_newton[local_min_indexes[-1],1,0], p_newton[local_min_indexes[-1],1,1], "ko")
+# plt.plot(perihelion_points[::100,0], perihelion_points[::100,1], "ko")
 plt.show()
