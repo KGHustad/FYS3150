889f6f1a6c69589e8d3187991875ae90810eee59
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-10-22 17:57:39 +0200

    Cleaned up Einstein

--- project3/EinsteinWasRightAllAlong.py
+++ project3/EinsteinWasRightAllAlong.py
@@ -4,6 +4,13 @@ import numpy as np
 import scipy.signal
 import math
 
+def find_perihelion(pos_star, pos_celbody):
+    dist = np.linalg.norm(pos_star - pos_celbody, axis=1)
+    local_minima_indices = scipy.signal.argrelmin(dist)
+    pos_minima = pos_celbody[local_minima_indices]
+    angles = np.arctan(pos_minima[:,1] /  pos_minima[:,0])
+    return pos_minima, angles, local_minima_indices
+
 mercurymass = 0.166e-6
 mercuryspeed = 12.44
 mercury_perihelion_distance = 0.3075
@@ -12,30 +19,30 @@ MySolarSystem = SolarSystem()
 MySolarSystem.CreateCelestialObject(0, 0, 0, 0, 1)
 MySolarSystem.CreateCelestialObject(mercury_perihelion_distance, 0, 0, mercuryspeed, mercurymass)
 
-p_newton, v_newton = MySolarSystem.fill_array_c(int(1e7), 100, skip_saving=10)
+N = int(1e7)    # total time steps stored
+years = 100
+p_newton, v_newton = MySolarSystem.fill_array_c(N, years#, skip_saving=10
+)
 
-p_einstein, v_einstein = MySolarSystem.fill_array_c(int(1e7), 100, acc_method=MySolarSystem.AccRelativistic, skip_saving=10)
+p_einstein, v_einstein = MySolarSystem.fill_array_c(N, years, acc_method=MySolarSystem.AccRelativistic#, skip_saving=10
+)
+
+time_steps = np.linspace(0, years, N+1)
 
 
+# newton
+res_newton = find_perihelion(p_newton[:,0,:], p_newton[:,1,:])
+points_newton, angles_newton, indices_newton = res_newton
+plt.plot(time_steps[indices_newton], angles_newton)
 
-local_min_indexes = scipy.signal.argrelextrema( np.sqrt((p_newton[:,1,0] - p_newton[:,0,0])**2 + (p_newton[:,1,1] - p_newton[:,0,1])**2), np.less )[0]
-perihelion_points_newton = np.zeros(shape=(len(local_min_indexes),2))
-perihelion_angle_newton = np.zeros(len(local_min_indexes))
-for i in range(len(local_min_indexes)):
-    perihelion_points_newton[i] = p_newton[local_min_indexes[i],1]
-    perihelion_angle_newton[i] = np.arctan( p_newton[local_min_indexes[i],1,1] / p_newton[local_min_indexes[i],1,0] )
-plt.plot(perihelion_angle_newton, "r-")
+# einstein
+res_einstein = find_perihelion(p_einstein[:,0,:], p_einstein[:,1,:])
+points_einstein, angles_einstein, indices_einstein = res_einstein
+plt.plot(time_steps[indices_einstein], angles_einstein)
 
-local_min_indexes = scipy.signal.argrelextrema( np.sqrt((p_einstein[:,1,0] - p_einstein[:,0,0])**2 + (p_einstein[:,1,1] - p_einstein[:,0,1])**2), np.less )[0]
-perihelion_angle_einstein = np.zeros(len(local_min_indexes))
-perihelion_points_einstein = np.zeros(shape=(len(local_min_indexes),2))
-for i in range(len(local_min_indexes)):
-    perihelion_points_einstein[i] = p_einstein[local_min_indexes[i],1]
-    perihelion_angle_einstein[i] = np.arctan( p_einstein[local_min_indexes[i],1,1] / p_einstein[local_min_indexes[i],1,0] )
-plt.plot(perihelion_angle_einstein, "b-")
 
 plt.title("Change in perihelion angle over a century")
-plt.legend(["Clasical Mechanics case", "Relativistic case"])
+plt.legend(["Classical Mechanics case", "Relativistic case"])
 plt.xlabel("time in years")
 plt.ylabel("angle in radians")
 plt.show()
@@ -46,12 +53,12 @@ plt.plot(p_newton[::100,1,0], p_newton[::100,1,1], "r-")
 plt.plot(p_einstein[::100,1,0], p_einstein[::100,1,1], "b-")
 plt.plot(p_newton[-1,1,0], p_newton[-1,1,1], "ro")
 plt.plot(p_einstein[-1,1,0], p_einstein[-1,1,1], "bo")
-plt.plot(perihelion_points_einstein[:,0], perihelion_points_einstein[:,1], "ko")
+plt.plot(points_einstein[:,0], points_einstein[:,1], "ko")
 plt.show()
 
-print perihelion_points_newton[0]
-print perihelion_points_newton[-1]
-print perihelion_angle_newton[-1]
-print perihelion_points_einstein[0]
-print perihelion_points_einstein[-1]
-print perihelion_angle_einstein[-1]
+print points_newton[0]
+print points_newton[-1]
+print angles_newton[-1]
+print points_einstein[0]
+print points_einstein[-1]
+print angles_einstein[-1]
