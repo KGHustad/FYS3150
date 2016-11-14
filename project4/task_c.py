5b47f311bddd3225d0bfa79547129fbc2d9b19e0
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-11-14 18:08:25 +0100

    Added multiprocessing

--- project4/task_c.py
+++ project4/task_c.py
@@ -1,9 +1,10 @@
 from common import *
+import multiprocessing
 
 L = 20
 seed = 3150*2
 
-mc_cycles = int(1E5)
+mc_cycles = int(1E6)
 J = 1
 save_every_nth = 1
 
@@ -11,13 +12,19 @@ spin_homogeneous = homogeneous_spin_matrix(L, 1)
 spin_random = random_spin_matrix(L, seed=seed)
 spin_matrices = [(spin_homogeneous, 'homogeneous'), (spin_random, 'random')]
 
+pool = multiprocessing.Pool()
 
 T_values = [1, 2.4]
-
+results = {}
 for original_spin, desc in spin_matrices:
     for T in T_values:
         spin = original_spin.copy()
-        energies, mean_magnetization, accepted_configurations, time_spent = metropolis_c(spin, J, T, mc_cycles, save_every_nth=save_every_nth, seed=seed)
+        argv = [spin, J, T, mc_cycles, save_every_nth, seed]
+        results[(desc, T)] = pool.apply_async(metropolis_c, argv)
+
+for original_spin, desc in spin_matrices:
+    for T in T_values:
+        energies, mean_magnetization, accepted_configurations, time_spent = results[(desc, T)].get()
 
         expectation_value_for_energy = expectation_values( energies, save_every_nth, mc_cycles )
         plt.plot(expectation_value_for_energy, 'r')
@@ -25,6 +32,7 @@ for original_spin, desc in spin_matrices:
         plt.ylabel("Expectation value for energy")
         plt.title("Converge of expectation values\nfor %.1e Monte-Carlo cycles\nwith a %s spin matrix" % (mc_cycles, desc))
         plt.tight_layout()
+        plt.savefig('plot_c_energy_%s_T=%g.pdf' % (desc, T))
         plt.show()
 
         expectation_value_for_magnetization = expectation_values( mean_magnetization, save_every_nth, mc_cycles )
@@ -33,4 +41,5 @@ for original_spin, desc in spin_matrices:
         plt.ylabel("Expectation value for magnetization")
         plt.title("Converge of expectation values\nfor %.1e Monte-Carlo cycles\nwith a %s spin matrix" % (mc_cycles, desc))
         plt.tight_layout()
+        plt.savefig('plot_c_magnetization_%s_T=%g.pdf' % (desc, T))
         plt.show()
