91dc7b5cc3c90b3aa1b928a8e6eb621921f0e03c
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-11-14 13:22:14 +0100

    Pretty prints

--- project4/test_c.py
+++ project4/test_c.py
@@ -8,26 +8,39 @@ J = 1
 T = 1
 save_every_nth = 1
 
-show_spins(spin)
+#show_spins(spin)
 
-mc_cycles = int(1E5)
+mc_cycles = int(1E7)
 energies, mean_magnetization, accepted_configurations, time_spent = metropolis_c(spin, J, T, mc_cycles, save_every_nth=save_every_nth)
 mu_E, mu_M, mu_abs_M, mu_E_sq, mu_M_sq = extract_expectation_values(energies, mean_magnetization)
 
 print mu_E, mu_M, mu_abs_M, mu_E_sq, mu_M_sq
 
-print len(energies)
 
-print energies
-print mean_magnetization
+exact_mean_energy = analytical_mean_energy(J, T)
+exact_mean_energy_sq = analytical_mean_energy_squared(J, T)
+exact_mean_magnetization = analytical_mean_magnetization(J, T)
 
-exact_energy = analytical_energy(J, T)
+print "Mean energy:             %8g" % mu_E
+print "Analytical mean energy:  %8g" % exact_mean_energy
+print "Error in mean energy:    %8g" % ((mu_E - exact_mean_energy)
+                                        /exact_mean_energy)
+print
+print "Mean energy squared:         %8g" % mu_E_sq
+print "Analytical mean energy sq.:  %8g" % exact_mean_energy_sq
+print "Error in mean energy sq.:    %8g" % ((mu_E_sq - exact_mean_energy_sq)
+                                            /exact_mean_energy_sq)
+print
+print "Mean abs. magnetization: %8g" % mu_abs_M
+print "Analytical mean mag.:    %8g" % exact_mean_magnetization
+print "Error in mean mag..:     %8g" % ((mu_M - exact_mean_magnetization)
+                                        /exact_mean_magnetization)
+print
+print "Mean magnetization: %8g" % mu_M
+print "Mean magnetization sq.:  %8g" % mu_M_sq
 
-print "Mean energy:         %8g" % mu_E
-print "Analytical Energy:   %8g" % exact_energy
-print "Energy error:        %8g" % ((mu_E - exact_energy)/exact_energy)
-print "Mean magnetization:  %8g" % mu_M
-print "Accepted configurations: %g" % accepted_configurations
+print
+print "Accepted configurations: %8g" % accepted_configurations
 #print spin
 
 show_spins(spin)
