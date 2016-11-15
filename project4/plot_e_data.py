5d62146ce37562b1893fea5076a580bfa75bb95a
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-11-15 19:02:35 +0100

    Add usage warning

--- project4/plot_e_data.py
+++ project4/plot_e_data.py
@@ -6,7 +6,12 @@ from task_e import plot
 
 filenames = sys.argv[1:]
 
+if len(filenames) == 0:
+    print "USAGE: python %s <data file> [more data files]" % (sys.argv[0])
+    sys.exit(1)
+
 for filename in filenames:
     with open(filename, 'r') as f:
         data_dict = pickle.load(f)
-    plot(data_dict, show=True)
+    plot(data_dict, #show=True
+    )
