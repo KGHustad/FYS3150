50854f4c3b88b4bbc7ca7accf7a72a9271fa49f7
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-11-17 09:44:25 +0100

    POST-DELIVERY (project 4): Removed unused code and added some comments

--- project4/src/common.py
+++ project4/src/common.py
@@ -7,6 +7,7 @@ import ctypes
 import math
 import os
 
+# utility functions for smart path handling
 def get_lib_name():
     return 'libising.so'
 
@@ -20,6 +21,7 @@ def get_proj_path():
     proj_path = os.path.abspath(os.path.join(this_file_dir, '..'))
     return proj_path
 
+# functions for generating lattices
 def homogeneous_spin_matrix(L, value=1):
     if not (value == 1 or value == -1):
         print "Spins must be -1 or 1"
@@ -47,40 +49,7 @@ def random_spin_matrix_c(L, seed=None):
                          c_ulong(seed))
     return spin
 
-def EnergyConfig(A, J):
-    L = A.shape[0]
-    Ei = 0
-    for i in range(L):
-        for j in range(L):
-            Ei += A[i,j] * A[i-1,j]
-            Ei += A[i,j] * A[i,j-1]
-    return -J*Ei
-
-def deltaE(A, J, i, j):
-    L = A.shape[0]
-    E_old_right = A[i,j] * A[(i+1)%L, j]
-    E_old_left = A[i,j] * A[i-1, j]
-    E_old_up = A[i,j] * A[i, (j+1)%L]
-    E_old_down = A[i,j] * A[i, j-1]
-    E_old = (E_old_right + E_old_left + E_old_up + E_old_down)
-
-    diff = J*2*E_old
-    return diff
-
-def Metropolis(A, J, steps):
-    L = A.shape[0]
-    Energy = np.zeros(steps+1)
-    Energy[0] = EnergyConfig(A, J)
-    for k in range(steps):
-        i = random.randint(0,L-1)
-        j = random.randint(0,L-1)
-        dE = deltaE(A, J, i, j)
-        if dE <= 0:
-            A[i,j] *= -1
-            Energy[k+1] = dE
-        Energy[k+1] += Energy[k]
-    return A, Energy
-
+# wrapper around the c solver
 def metropolis_c(spin, J, T, sweeps, save_every_nth=1, seed=0, silent=False):
     if not spin.dtype == np.int8:
         print "Wrong usage! Spin array must be of dtype np.int8"
@@ -143,6 +112,7 @@ def metropolis_c(spin, J, T, sweeps, save_every_nth=1, seed=0, silent=False):
     mean_magnetization = abs(tot_magnetization)
     return energies, mean_magnetization, accepted_configurations.value, time_spent
 
+# utility functions
 def extract_expectation_values(energies, mean_magnetization):
     mu_E = np.mean(energies)
     mu_M = np.mean(mean_magnetization)
@@ -150,9 +120,6 @@ def extract_expectation_values(energies, mean_magnetization):
     mu_E_sq = np.mean(energies**2)
     mu_M_sq = np.mean(mean_magnetization**2)
     return mu_E, mu_M, mu_abs_M, mu_E_sq, mu_M_sq
-
-def Magnetization(A):
-    return abs(np.sum(A))
 
 def show_spins(spin):
     plt.imshow(spin, cmap=plt.cm.gray, vmin=-1, vmax=1, interpolation='none')
