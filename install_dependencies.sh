5d3343e7876a4e6837b8f23d0013c140fbc56346
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-11-24 03:06:51 +0100

    Update install_depencies with more comments and add MPI stuff

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,16 @@
+# tabulate is used for pretty printing tables
 sudo -H pip install tabulate
+
+# The GNU Scientific Library is used for RNGs
 sudo apt-get install libgsl-dev -y
+
+# GSL should be linked to atlas for decent linear algebra performance (probably irrelevant for our use)
 sudo apt-get install libatlas-base-dev -y
+
+
+# MPI
+# OpenMPI
+sudo apt-get install openmpi-bin openmpi-doc libopenmpi-dev -y
+
+# mpi4py for using MPI from Python
+sudo -H pip install mpi4py
