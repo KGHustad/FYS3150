00615cc25ed96caef474011d784c164fcb16bb90
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-11-26 01:08:11 +0100

    Add function for copying selected parts of an array

--- project5/src/c/common.c
+++ project5/src/c/common.c
@@ -22,6 +22,18 @@ void memcpy_2d_array(double **dest, double **src, int rows, int columns) {
     memcpy(dest[0], src[0], sizeof(double)*rows*columns);
 }
 
+void copy_selection_2d_array(double **dest, double **src,
+                             int dest_row_offset, int dest_col_offset,
+                             int src_row_offset, int src_col_offset,
+                             int rows, int cols) {
+    int row;
+    for (row=0; row < rows; row++) {
+        memcpy(dest[row + dest_row_offset]+dest_col_offset,
+               src[row + src_row_offset]+src_col_offset,
+               sizeof(double)*cols);
+    }
+}
+
 void free_2d_array(double **a) {
     free(a[0]);
     free(a);
--- project5/src/c/common.h
+++ project5/src/c/common.h
@@ -1,4 +1,8 @@
 double** alloc_2d_array_from_flat(double *a_flat, int rows, int columns);
 double** alloc_2d_array(int rows, int columns);
 void memcpy_2d_array(double **dest, double **src, int rows, int columns);
+void copy_selection_2d_array(double **dest, double **src,
+                             int dest_row_offset, int dest_col_offset,
+                             int src_row_offset, int src_col_offset,
+                             int rows, int cols);
 void free_2d_array(double **a);
