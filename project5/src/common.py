5b0189a9f77b929c9dd00cef0a00117d4a8e8255
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-12-02 00:33:58 +0100

    src/plotting.py
    Swift handling of fig dir creation and give warning if lib file doesn't exist

--- project5/src/common.py
+++ project5/src/common.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import sys
 import ctypes
 import time
 
@@ -12,10 +13,24 @@ def get_lib_path():
     relative_lib_path = os.path.join(this_file_dir, 'c')
     return relative_lib_path
 
+def check_lib_exists():
+    lib_file = os.path.join(get_lib_path(), get_lib_name())
+    if not os.path.isfile(lib_file):
+        print "ERROR: Cannot find the library file '%s'" % lib_file
+        print "Try to make the library with 'make diffusion_lib'"
+        sys.exit(1)
+
 def get_proj_path():
     this_file_dir = os.path.dirname(__file__)
+    # assume this file lies in <project_dir>/src
     proj_path = os.path.abspath(os.path.join(this_file_dir, '..'))
     return proj_path
+
+def ensure_fig_dir():
+    proj_path = get_proj_path()
+    fig_dir = os.path.join(proj_path, 'fig')
+    if not os.path.isdir(fig_dir):
+        os.mkdir(fig_dir)
 
 def load_lib():
     return np.ctypeslib.load_library(get_lib_name(), get_lib_path())
@@ -25,6 +40,7 @@ def load_lib_alt():
 
 def diffusion_2d(v, f, iterations, kappa, bc_left=0, bc_right=0,
                  bc_top=0, bc_bottom=0, omp=False, silent=False):
+    check_lib_exists()
     #libdiffuse = load_lib()
     libdiffuse = load_lib_alt()
 
--- project5/src/plotting.py
+++ project5/src/plotting.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
+from common import ensure_fig_dir
 
 def show_2d(v):
     plt.imshow(v, cmap=plt.cm.gray, vmin=0, vmax=1, interpolation='none')
     plt.show()
+
+ensure_fig_dir()
