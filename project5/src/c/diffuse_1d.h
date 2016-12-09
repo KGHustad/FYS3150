714b170ea58a48f262b05b423e79a859367c025f
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-12-09 17:49:25 +0100

    Forgot to add header file

--- project5/src/c/diffuse_1d.h
+++ project5/src/c/diffuse_1d.h
@@ -1,1 +1,6 @@
 enum solver {FORWARD_EULER, BACKWARD_EULER, CRANK_NICOLSON};
+
+void diffusion_1d_forward_euler(double *v, double alpha, int n, int iters);
+void diffusion_1d_backward_euler(double *v, double alpha, int n, int iters);
+void diffusion_1d_crank_nicolson(double *v, double alpha, int n, int iters);
+void solve_1d(double *v, double alpha, int n, int iters, enum solver s);
