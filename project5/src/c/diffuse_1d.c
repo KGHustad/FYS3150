17322968e7d05b645edf44906c232f618c7d6960
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-12-09 16:18:42 +0100

    Crank-Nicolson should be good

--- project5/src/c/diffuse_1d.c
+++ project5/src/c/diffuse_1d.c
@@ -80,7 +80,48 @@ void diffusion_1d_backward_euler(double *v, double alpha, int n, int iters) {
 }
 
 void diffusion_1d_crank_nicolson(double *v, double alpha, int n, int iters) {
-    /* NEEDS TO BE IMPLEMENTED */
+    int i, it;
+    size_t data_size = (n+2)*sizeof(double);
+    double *a = malloc(data_size);
+    double *b = malloc(data_size);
+    double *c = malloc(data_size);
+
+    /* create buffer array */
+    double *v_buf = malloc(data_size);
+    double *v_new = v_buf;
+    double *v_tmp;
+
+    /* save destination array */
+    double *v_dest = v;
+
+
+    for (it = 0; it < iters; it++) {
+        for (i = 0; i <= n+1; i++) {
+            a[i] = -alpha;
+            b[i] = 2 + 2*alpha;
+            c[i] = -alpha;
+        }
+
+        for (i = 1; i <= n; i++) {
+            v[i] = alpha*v_new[i-1] + (2 - 2*alpha)*v_new[i] + alpha*v_new[i+1];
+        }
+
+        solve_tridiagonal(v_new, v, a, b, c, n);
+
+        /* swap pointers */
+        //v_tmp = v_new;
+        //v_new = v;
+        //v = v_tmp;
+    }
+
+    if (v_dest != v_new) {
+        memcpy(v_dest, v_new, (n+2)*sizeof(double));
+    }
+
+    free(a);
+    free(b);
+    free(c);
+    free(v_buf);
 }
 
 void solve_1d(double *v, double alpha, int n, int iters, enum solver s) {
