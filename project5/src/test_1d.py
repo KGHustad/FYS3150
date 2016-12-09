b99f8ef4d575ef3d52e78ef8c584e6a0f9d3aba1
Jonas Gahr Sturtzel Lunde <jonas.s.lunde@outlook.com>
2016-12-09 20:54:28 +0100

    1d stuff

--- project5/src/test_1d.py
+++ project5/src/test_1d.py
@@ -29,46 +29,10 @@ for n in [10,100]:
             x = np.linspace(0, 1, n+2)
             plt.plot(x, v, label=solver)
 
-        analytical_solution = analytical_u_1d(x,T)
-
         plt.plot(x, analytical_solution)
         plt.rc('font', **{'size' : 14})
         plt.tight_layout()
         plt.title("1D case after T=%.2f, with dx=%.2f and dt=%.5f" % (T,dx,dt))
-        plt.xlabel("x")
-        plt.ylabel("u(x,t)")
-        plt.legend(loc='best')
-        plt.show()
-
-
-
-
-for n in [10,100]:
-    for T in [0.05,0.5]:
-        N = stable_N(n)
-        dx = 1./n
-        dt = T/float(N)
-        kappa = dt/dx**2
-        print "dx: %g  dt: %g  kappa: %g" % (dx, dt, kappa)
-        u = np.zeros(n+2)
-        u[-1] = 1
-
-        for solver in SOLVERS_1D:
-            v = u.copy()
-            diffusion_1d(v, N, kappa, solver)
-            print "Solver: %s" % solver
-            print v
-            print
-
-            x = np.linspace(0, 1, n+2)
-            analytical_solution = analytical_u_1d(x,T)
-            error = abs(analytical_solution - v)
-            plt.plot(x, error, label=solver)
-
-
-        plt.rc('font', **{'size' : 14})
-        plt.tight_layout()
-        plt.title("Error for T=%.2f, with dx=%.2f and dt=%.5f" % (T,dx,dt))
         plt.xlabel("x")
         plt.ylabel("u(x,t)")
         plt.legend(loc='best')
