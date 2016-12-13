43c7f793363ef22913da38c6ec1e649da127005a
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-12-13 18:36:12 +0100

    Wrote stuff

--- project5/src/plotting.py
+++ project5/src/plotting.py
@@ -1,8 +1,14 @@
 import matplotlib.pyplot as plt
 from common import ensure_fig_dir
 
-def show_2d(v):
+def show_2d(v, title=None, save_to=None, show=True):
     plt.imshow(v, cmap=plt.cm.gray, vmin=0, vmax=1, interpolation='none')
-    plt.show()
+    if title is not None:
+        plt.title(title)
+    if save_to is not None:
+        plt.savefig(save_to)
+    if show:
+        plt.show()
+    plt.clf()
 
 ensure_fig_dir()
--- project5/src/test_2d.py
+++ project5/src/test_2d.py
@@ -1,27 +1,38 @@
 from common import *
 from plotting import *
+import sys
 
-iterations = int(1E4)
+show = True
+if '--no_show' in sys.argv:
+    show = False
 
-height = 400
-width = 400
+iterations = int(1E5)
+
+width = 100
+height = width
 kappa = 1E-2
+
+dx = 1./width
+dt = kappa*dx**2
+T = dt*iterations
+
+figdir = get_fig_dir()
 
 v = np.zeros((height, width), dtype=np.float64)
 
 v[:,-1] = 1
 
-diffusion_2d(v, iterations, kappa, bc_left=1, bc_top=1, bc_bottom=1)
+diffusion_2d(v, iterations, kappa)
 #print v
+title ="$T = %g$,  $\\Delta t=%f$" % (T, dt)
+print title
+show_2d(v, title=title,
+        save_to=figdir+"/plot_2d_all_dirichlet.pdf", show=show)
 
-show_2d(v)
 
 
-
-# uniform spread
+# linear, isolated top and bottom
 v = np.zeros((height, width), dtype=np.float64)
-
-uniform = np.linspace(0, 1, width)
-v[:] = uniform[:]
-diffusion_2d(v, iterations, kappa, bc_left=1, bc_top=1, bc_bottom=1, bc_right=1)
-show_2d(v)
+v[:,-1] = 1
+diffusion_2d(v, iterations, kappa, bc_left=0, bc_top=1, bc_bottom=1, bc_right=0)
+show_2d(v, save_to=figdir+"/plot_2d_dirichlet_x_and_neumann_y.pdf", show=show)
