4f44a02463c4910973abdbdf08545635b250151d
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-12-13 04:00:15 +0100

    Improve memtests

--- project5/src/c/memtest_1d.c
+++ project5/src/c/memtest_1d.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "diffuse_1d.h"
 
 int main(int argc, char *argv[]) {
@@ -6,13 +7,28 @@ int main(int argc, char *argv[]) {
     int num_solvers = 3;
 
     int n = 4;
-    double *v = malloc(sizeof(double)*(n+2));
     double alpha = 0.1;
-
+    double *v = malloc(sizeof(double)*(n+2));
     int i;
+    for (i = 0; i < n+2; i++) {
+        v[i] = i/(double)(n+1);
+    }
+
     for (i = 0; i < num_solvers; i++) {
         solve_1d(v, alpha, n, 2, solvers[i]);
         solve_1d(v, alpha, n, 1, solvers[i]);
+    }
+
+    double sum = 0;
+    for (i = 0; i < n+2; i++) {
+        sum += v[i];
+    }
+
+    /* trigger 'conditional jump depends on uninitialized values' by using a
+     * variable's value
+     */
+    if (sum == 0) {
+        printf("%g\n", sum);
     }
 
     free(v);
--- project5/src/c/memtest_2d_omp.c
+++ project5/src/c/memtest_2d_omp.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "diffuse_2d_omp.h"
 
 int main(int argc, char *argv[]) {
@@ -9,8 +10,25 @@ int main(int argc, char *argv[]) {
     int bc = 0;
     double time_spent;
 
+    int i;
+    for (i = 0; i < height*width; i++) {
+        v[i] = i/(double)(height*width);
+    }
+
     solve_2d_omp(v, height, width, alpha, 1, bc, bc, bc, bc, &time_spent);
     solve_2d_omp(v, height, width, alpha, 2, bc, bc, bc, bc, &time_spent);
+
+    double sum = 0;
+    for (i = 0; i < height*width; i++) {
+        sum += v[i];
+    }
+
+    /* trigger 'conditional jump depends on uninitialized values' by using a
+     * variable's value
+     */
+    if (sum == 0) {
+        printf("%g\n", sum);
+    }
 
     free(v);
 
