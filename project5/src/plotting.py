811da561b4c04bbf5488ae795024aa05f249b23d
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-12-13 20:28:24 +0100

    More or less done

--- project5/src/plotting.py
+++ project5/src/plotting.py
@@ -3,6 +3,8 @@ from common import ensure_fig_dir
 
 def show_2d(v, title=None, save_to=None, show=True):
     plt.imshow(v, cmap=plt.cm.gray, vmin=0, vmax=1, interpolation='none')
+    plt.xlabel('$i$')
+    plt.ylabel('$j$')
     if title is not None:
         plt.title(title)
     if save_to is not None:
