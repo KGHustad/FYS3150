53613f1d1f28966ad56032ff863b7177d6930f55
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2016-12-20 03:19:30 +0100

    Proper fix of tests (setting the seed defeats the purpose of testing with random input)

--- project5/src/tests.py
+++ project5/src/tests.py
@@ -1,12 +1,10 @@
 from common import *
 
 def _test_solve_1d(solver, LHS_formula, RHS_formula):
-    np.random.seed(3150)
-
     silent = True
     n = 4
     alpha = 0.1
-    atol = 0
+    atol = 1e-16
     rtol = 1e-15
 
     v_original = np.random.random(n+2)
@@ -18,8 +16,10 @@ def _test_solve_1d(solver, LHS_formula, RHS_formula):
 
     LHS = LHS_formula(v_new, alpha)
     RHS = RHS_formula(v, alpha)
-    msg = "Arrays are not almost equal! Difference:\n" + str(LHS - RHS)
-    assert np.allclose(LHS, RHS, atol=0, rtol=rtol), msg
+    msg = "Arrays are not almost equal!\n"
+    msg += "Absolute difference:\n%s\n" % str(abs(LHS - RHS))
+    msg += "Relative difference:\n%s\n" % str(abs((LHS - RHS)/RHS))
+    assert np.allclose(LHS, RHS, atol=atol, rtol=rtol), msg
 
     # check that 2 iterations equal 1 + 1 iterations
     v_a = v_original.copy()
@@ -27,7 +27,7 @@ def _test_solve_1d(solver, LHS_formula, RHS_formula):
     diffusion_1d(v_a, 1, alpha, solver=solver, silent=silent)
     diffusion_1d(v_a, 1, alpha, solver=solver, silent=silent)
     diffusion_1d(v_b, 2, alpha, solver=solver, silent=silent)
-    msg = "Arrays are not equal! Difference:\n" + str(LHS - RHS)
+    msg = "Arrays are not equal! Difference:\n" + str(v_a - v_b)
     assert np.array_equal(v_a, v_b), msg
 
 def test_solve_1d_forward_euler():
@@ -54,12 +54,10 @@ def test_solve_1d_crank_nicolson():
     _test_solve_1d('crank_nicolson', LHS, RHS)
 
 def _test_solve_2d(solver, omp):
-    np.random.seed(3150)
-
     silent = True
     n = 4
     alpha = 0.1
-    atol = 0
+    atol = 1e-16
     rtol = 1e-15
 
     v_original = np.random.random((n+2, n+2))
@@ -74,7 +72,10 @@ def _test_solve_2d(solver, omp):
           + alpha*(                     v[0:-2, 1:-1]
                   + v[1:-1, 0:-2]   - 4*v[1:-1, 1:-1]    + v[1:-1, 2:]
                                     +   v[2:,   1:-1])
-    assert np.allclose(LHS, RHS, atol=0, rtol=rtol)
+    msg = "Arrays are not almost equal!\n"
+    msg += "Absolute difference:\n%s\n" % str(abs(LHS - RHS))
+    msg += "Relative difference:\n%s\n" % str(abs((LHS - RHS)/RHS))
+    assert np.allclose(LHS, RHS, atol=atol, rtol=rtol), msg
 
     # check that 2 iterations equal 1 + 1 iterations
     v_a = v_original.copy()
@@ -82,7 +83,8 @@ def _test_solve_2d(solver, omp):
     solver(v_a, 1, alpha, omp=omp, silent=silent)
     solver(v_a, 1, alpha, omp=omp, silent=silent)
     solver(v_b, 2, alpha, omp=omp, silent=silent)
-    assert np.array_equal(v_a, v_b)
+    msg = "Arrays are not equal! Difference:\n" + str(v_a - v_b)
+    assert np.array_equal(v_a, v_b), msg
 
 def test_solve_2d_serial():
     _test_solve_2d(diffusion_2d, omp=False)
