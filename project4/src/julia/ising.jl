739d68f060736ad7968a5dc2046240fc7015ccd9
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2018-01-20 17:09:17 +0100

    Project 4: Split julia library and benchmark into separate files

--- project4/src/julia/ising.jl
+++ project4/src/julia/ising.jl
@@ -116,27 +116,3 @@ function solve(lat::Lattice, sweeps::Int64, J::Float64, T::Float64,
     metropolis(lat, sweeps, J, energies, tot_magnetization, r, dE_cache,
                 save_every_nth)
 end
-
-function bench()
-    J = 1.
-    T = 2.27
-
-    L = 100
-    sweeps = 10000
-
-    spin = Array{Int8, 2}(L, L)
-    for i = 1:L
-        for j = 1:L
-            spin[i,j] = 1
-        end
-    end
-    energies = Array{Float64}(sweeps+1)
-    tot_magnetization = Array{Int64}(sweeps+1)
-    save_every_nth = 1
-    seed = 0
-    lat = Lattice(spin, L, 0, 0, 0)
-
-    solve(lat, sweeps, J, T, energies, tot_magnetization, save_every_nth, seed)
-end
-
-@time bench()
