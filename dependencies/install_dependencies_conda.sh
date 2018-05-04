765628a04d3c9ddec329ba3307e2b6cfa949303e
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2018-05-04 17:21:42 +0200

    Install clangdev with conda

--- dependencies/install_dependencies_conda.sh
+++ dependencies/install_dependencies_conda.sh
@@ -1,7 +1,7 @@
 env_prefix=$(python -c "import os, sys; conda_bin=os.path.dirname(sys.executable); print (os.path.normpath(os.path.join(conda_bin, '..')))")
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-conda install -y numpy scipy weave matplotlib pytest gsl openmp
+conda install -y numpy scipy weave matplotlib pytest gsl clangdev openmp
 
 # copy in scripts that update environment variables to ensure that libraries installed in the environment (really just GSL in this case) are detected
 mkdir -p $env_prefix/etc/conda/activate.d && cp $DIR/conda_activate.sh $env_prefix/etc/conda/activate.d/lib.sh
