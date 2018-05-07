b7c3c49e5a5613afc0fbd7d865bb6f9e1121225f
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2018-05-07 18:25:56 +0200

    Only trigger weave compilation if weave is installed

--- project2/src/trigger_weave_compilation.py
+++ project2/src/trigger_weave_compilation.py
@@ -1,5 +1,8 @@
 from common import *
 
 if __name__ == '__main__':
-    A = np.zeros((2, 2), dtype=np.float64)
-    find_max_nondiagonal_symmetrical_weave(A)
+    if weave_imported:
+        A = np.zeros((2, 2), dtype=np.float64)
+        find_max_nondiagonal_symmetrical_weave(A)
+    else:
+        print "WARNING: Weave is not installed"
